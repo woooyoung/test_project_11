d0baf3c9d24627c48ef61ac79f19d39e6decb407
woooyoung <axdsw121@gmail.com>
2024-11-04 15:49:34 +0900

    Http와 String

--- src/main/java/com/koreait/surl_project_11/HomeController.java
+++ src/main/java/com/koreait/surl_project_11/HomeController.java
@@ -2,20 +2,33 @@ package com.koreait.surl_project_11;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("a")
     @ResponseBody
-    public String hi() {
-        return "hi";
+    public String hi(
+            String age,
+            String id
+    ) {
+        return "안녕, %s번, %s살이야.".formatted(id, age);
     }
 
     @GetMapping("b")
     @ResponseBody
-    public String hi2() {
-        return "hi2";
+    public String plus(
+            @RequestParam("a") String num1Str,
+            String b
+    ) {
+        int num1 = Integer.parseInt(num1Str);
+        int num2 = Integer.parseInt(b);
+
+        System.out.println("a : " + num1);
+        System.out.println("b : " + num2);
+
+        return "a + b = %d".formatted(num1 + num2);
     }
 
 }
