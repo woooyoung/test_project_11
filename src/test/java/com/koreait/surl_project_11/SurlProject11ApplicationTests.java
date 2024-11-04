57a38c81162f440a58e8150ffeda7e717718fed0
woooyoung <axdsw121@gmail.com>
2024-11-04 15:12:58 +0900

    프로젝트 세팅

---
+++ src/test/java/com/koreait/surl_project_11/SurlProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.surl_project_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurlProject11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
