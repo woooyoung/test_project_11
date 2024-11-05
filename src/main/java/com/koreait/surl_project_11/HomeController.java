f40406dbb59cfcc95028d7baae6aac8500ca52d1
woooyoung <axdsw121@gmail.com>
2024-11-05 11:14:53 +0900

    리턴타입 테스트

--- src/main/java/com/koreait/surl_project_11/HomeController.java
+++ src/main/java/com/koreait/surl_project_11/HomeController.java
@@ -19,16 +19,100 @@ public class HomeController {
     @GetMapping("b")
     @ResponseBody
     public String plus(
-            @RequestParam("a") String num1Str,
-            String b
+            //http://localhost:8080/b?a=20&b=40
+            @RequestParam("a") int num1,
+            @RequestParam("b") int num2,
+            @RequestParam(name = "c", defaultValue = "10") int num3
     ) {
-        int num1 = Integer.parseInt(num1Str);
-        int num2 = Integer.parseInt(b);
 
         System.out.println("a : " + num1);
         System.out.println("b : " + num2);
 
-        return "a + b = %d".formatted(num1 + num2);
+        return "a + b + c = %d".formatted(num1 + num2 + num3);
     }
 
+    @GetMapping("c")
+    @ResponseBody
+    public String c(
+            boolean married
+    ) {
+        return married ? "기혼" : "미혼";
+    }
+
+    @GetMapping("d")
+    @ResponseBody
+    public String d(
+            Boolean married
+    ) {
+        if (married == null) return "정보 입력해";
+
+        return married ? "기혼" : "미혼";
+    }
+
+    public static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+
+    @GetMapping("person1")
+    @ResponseBody
+    public String person1(
+            String name,
+            int age
+    ) {
+        Person person = new Person(name, age);
+
+        return person.toString();
+    }
+
+    @GetMapping("person2")
+    @ResponseBody
+    public String person2(
+            Person person
+    ) {
+        return person.toString();
+    }
+
+    @GetMapping("e")
+    @ResponseBody
+    public int e() {
+        int age = 10;
+        return age;
+    }
+
+    @GetMapping("f")
+    @ResponseBody
+    public int f() {
+        int age = 10;
+        return age;
+    }
 }
